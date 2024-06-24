aebf7e7baecbed431005a8611fd16b0580b9b843
TheoRybalov <fedorrybalov@gmail.com>
2024-06-24 17:52:16 +0300

    Добавлен метод для получения скриншота кнопки "Смотреть видео о платформе" в класс HomePage

--- src/test/java/Efficiency/Pages/HomePage.java
+++ src/test/java/Efficiency/Pages/HomePage.java
@@ -4,10 +4,12 @@ import Efficiency.CommonFunctions;
 import com.codeborne.selenide.SelenideElement;
 import org.testng.Assert;
 
+import java.io.IOException;
 import java.util.HashMap;
 
 import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selenide.$x;
+import static com.codeborne.selenide.Selenide.sleep;
 
 public class HomePage extends CommonFunctions {
 
@@ -18,6 +20,7 @@ public class HomePage extends CommonFunctions {
     private static final SelenideElement EnterpriseLink = $x("//*[@id=\"root\"]/div/header/div[2]/div/nav/ul/li[3]/a");
     private static final SelenideElement ProvidersLink = $x("//*[@id=\"root\"]/div/header/div[2]/div/nav/ul/li[4]/a");
     private static final SelenideElement AboutProjectLink = $x("//*[@id=\"root\"]/div/header/div[2]/div/nav/ul/li[5]/span/span");
+    public static final SelenideElement VideoAboutPlatformButton = $x("//*[@id=\"root\"]/div/main/section[1]/div/div/div[1]/div[4]");
     private HashMap<String, SelenideElement> selenideElementHashMap;
     public SelenideElement getHeaderElement(String key) {
         //Creating hash-map, like key-value List to get elements
@@ -32,22 +35,44 @@ public class HomePage extends CommonFunctions {
         return selenideElementHashMap.get(key).shouldBe(visible);
     }
 
+    public void TakeScreenshotOfFullPage(String environment) throws IOException {
+        String screenshotPath = null;
 
+        switch (environment) {
+            case "PC":
+                //Если мы получаем скриншот в кофигурации ПК, то сохранение будет идти в эту папку
+                screenshotPath = "src/test/resources/screenshots/industries/PC/current.png";
+                break;
+            case "phone":
+                //Если мы получаем скриншот в кофигурации телефон, то сохранение будет идти в эту папку
+                screenshotPath = "src/test/resources/screenshots/industries/phone/current.png";
+                break;
+            default:
+                throw new IllegalArgumentException("Неверный параметр окружения: " + environment);
+        }
 
-    public void ServicesLinkIsCorrect(){
-        Assert.assertEquals(getHeaderElement("servicesLink").getText(), "эффективность.рф");
+        //вызывается метод создания и сохранения такого скриншота, куда мы передаём путь до папки
+        super.TakeScreenshotOfFullPage(screenshotPath);
     }
 
-    public void IndustriesLinkIsCorrect(){
-        Assert.assertEquals(getHeaderElement("industriesLink").getText(), "эффективность.рф");
-    }
+    public void TakeScreenshotOfVideoButton(String environment) throws IOException {
+        String screenshotPath = null;
+        sleep(2000);
+        switch (environment) {
+            case "PC":
+                //Если мы получаем скриншот в кофигурации ПК, то сохранение будет идти в эту папку
+                screenshotPath = "src/test/resources/screenshots/HomePage/PC/Elements/current_video_button.png";
+                break;
+            case "phone":
+                //Если мы получаем скриншот в кофигурации телефон, то сохранение будет идти в эту папку
+                screenshotPath = "src/test/resources/screenshots/HomePage/PC/Elements/reference_video_button.png";
+                break;
+            default:
+                throw new IllegalArgumentException("Неверный параметр окружения: " + environment);
+        }
 
-    public void EnterpriseLinkIsCorrect(){
-        Assert.assertEquals(getHeaderElement("enterpriseLink").getText(), "эффективность.рф");
-    }
-
-    public void ProvidersLinkIsCorrect(){
-        Assert.assertEquals(getHeaderElement("providersLink").getText(), "эффективность.рф");
+        //вызывается метод создания и сохранения такого скриншота, куда мы передаём путь до папки
+        super.TakeScreenshotOfElement(VideoAboutPlatformButton, screenshotPath);
     }
 
 
--- src/test/java/Efficiency/Tests/HOME.java
+++ src/test/java/Efficiency/Tests/HOME.java
@@ -1,6 +1,24 @@
 package Efficiency.Tests;
 
+import Efficiency.Pages.HomePage;
+import Efficiency.Pages.IndustriesPage;
+import Efficiency.Providers.ConfigProviderInterface;
 import Efficiency.TestBase;
+import org.testng.annotations.Test;
+
+import java.io.IOException;
+
+import static com.codeborne.selenide.Selenide.open;
 
 public class HOME extends TestBase {
+
+    public String getEnvironment(){
+        return super.getEnv();
+    }
+
+    @Test(priority = 1, description = "Домашняя страница. Изменение кнопки 'Смотреть видео о платформе' ")
+    public void PlatesCorrectData_TEST() throws IOException {
+        HomePage homePage = open(ConfigProviderInterface.baseURL, HomePage.class);
+
+    }
 }
