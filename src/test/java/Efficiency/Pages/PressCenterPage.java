f8ea8c3804166c25784d55045149644c053ecf63
TheoRybalov <fedorrybalov@gmail.com>
2024-06-26 23:10:39 +0300

    Добавлены методы в PressCenterPage:
    PressCenterPage.getFirstLabelOfNewsPlatform - получает заголовок перовй новости в разделе "Новости платформы"
    PressCenterPage.getIdFromApi - метод, получающий по заголовку id новости в БД
    PressCenterPage.getNewsTitleFromDatabase - получает по id заголовок новости в БД
    PressCenterPage.compareNews - сравнивает зоголовки Новостей с сайта и из БД

--- src/test/java/Efficiency/Pages/PressCenterPage.java
+++ src/test/java/Efficiency/Pages/PressCenterPage.java
@@ -1,6 +1,12 @@
 package Efficiency.Pages;
 
 import Efficiency.CommonFunctions;
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.SelenideElement;
+import io.qameta.allure.Step;
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import org.testng.Assert;
 
 import java.sql.*;
 import java.util.ArrayList;
@@ -8,11 +14,39 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Selenide.$x;
+
 public class PressCenterPage extends CommonFunctions {
 
-    public List<Map<String, Object>> getNewsInformationById(long contentItemId) throws SQLException {
-        String query = "SELECT * FROM public.content_630 WHERE content_item_id = ?";
-        List<Map<String, Object>> itemList = new ArrayList<>();
+    private static final SelenideElement News_Platform_1 = $x("//*[@id=\"news-platform\"]/div/div/div[1]/a/div/div[2]/div[1]");
+    private static final SelenideElement News_Platform_2 = $x("//*[@id=\"news-platform\"]/div/div/div[2]/a/div/div[2]/div[1]");
+    private static final SelenideElement News_Platform_3 = $x("//*[@id=\"news-platform\"]/div/div/div[3]/a/div/div[2]/div[1]");
+
+    @Step("Получить первый заголовок новостей с платформы")
+    public String getFirstLabelOfNewsPlatform(){
+        return News_Platform_1.shouldBe(visible).getText();
+    }
+
+    @Step("Получить ID новости с API по заголовку: {title}")
+    public long getIdFromApi(String title) {
+        Response response = RestAssured.get("https://aksis.dev.qsupport.ru/api/PressCenter/GetNews?newsTypes=platformNews&pageSize=3");
+        Assert.assertFalse(response.asString().isEmpty(), "Response is empty");
+
+        List<Map<String, Object>> items = response.jsonPath().getList("items");
+        // Ищем элемент с заданным title
+        long id = items.stream()
+                .filter(item -> title.equals(item.get("title")))
+                .map(item -> ((Number) item.get("id")).longValue()) // Извлекаем значение "id" и преобразуем в long
+                .findFirst()
+                .orElseThrow(() -> new AssertionError("Информация не найдена"));
+
+        return id;
+    }
+
+    @Step("Получить информацию о новости из базы данных по ID: {contentItemId}")
+    public String getNewsTitleFromDatabase(long contentItemId) throws SQLException {
+        String query = "SELECT title FROM public.content_630 WHERE content_item_id = ?";
 
         try (Connection conn = DriverManager.getConnection(DB_URL, DB_USER, DB_PASSWORD);
              PreparedStatement pstmt = conn.prepareStatement(query)) {
@@ -20,22 +54,23 @@ public class PressCenterPage extends CommonFunctions {
             pstmt.setLong(1, contentItemId);
 
             try (ResultSet rs = pstmt.executeQuery()) {
-                while (rs.next()) {
-                    Map<String, Object> row = new HashMap<>();
-                    row.put("content_item_id", rs.getLong("content_item_id"));
-                    row.put("title", rs.getString("title"));
-                    row.put("description", rs.getString("description"));
-                    row.put("image", rs.getString("image"));
-
-                    itemList.add(row);
+                if (rs.next()) {
+                    return rs.getString("title");
                 }
             }
         } catch (SQLException e) {
             e.printStackTrace();
         }
 
-        return itemList;
+        throw new AssertionError("News title not found for contentItemId: " + contentItemId);
     }
+
+    @Step("Сравнить новость с сайта и из базы данных")
+    public void compareNews(String titleFromSite, String titleFromDB) throws SQLException {
+        Assert.assertEquals(titleFromSite, titleFromDB, "Titles do not match");
+    }
+
+
 
 
 }
--- src/test/java/Efficiency/Tests/PRESS_CENTER.java
+++ src/test/java/Efficiency/Tests/PRESS_CENTER.java
@@ -1,6 +1,27 @@
 package Efficiency.Tests;
 
+import Efficiency.Pages.PressCenterPage;
+import Efficiency.Providers.ConfigProviderInterface;
 import Efficiency.TestBase;
+import org.testng.annotations.Test;
+
+import java.io.IOException;
+import java.sql.SQLException;
+import java.util.List;
+import java.util.Map;
+
+import static com.codeborne.selenide.Selenide.open;
 
 public class PRESS_CENTER extends TestBase {
+
+    @Test(priority = 2, description = "Пресс-Центр. Новости платформы. Проверка заголовков новостей")
+    public void PlatformNews_TEST() throws IOException, SQLException {
+        PressCenterPage pressCenterPage = open(ConfigProviderInterface.pressCenterURL, PressCenterPage.class);
+        String newsTitleFromSite = pressCenterPage.getFirstLabelOfNewsPlatform();
+        long id = pressCenterPage.getIdFromApi(newsTitleFromSite);
+        String newsTitleFromDB = pressCenterPage.getNewsTitleFromDatabase(id);
+
+        pressCenterPage.compareNews(newsTitleFromSite, newsTitleFromDB);
+
+    }
 }
