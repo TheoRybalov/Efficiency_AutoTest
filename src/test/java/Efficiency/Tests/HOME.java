e4d8b497f60cd160fbb989bc0bd180e9ee6bca4d
peachlex <108018417+peachlex@users.noreply.github.com>
2024-07-01 15:59:20 +0300

    Revert "Добавлены проверки редиректов 'Все новости' и 'Все вопросы' на домашней странице"
    This reverts commit a4708351717757824ab6a5e7fa42d2be19b3c91e.

--- src/test/java/Efficiency/Tests/HOME.java
+++ src/test/java/Efficiency/Tests/HOME.java
@@ -81,16 +81,5 @@ public class HOME extends TestBase {
     }
 
 
-    @Test(priority = 9, description = "Домашняя страница. Проверка редиректа в блоке 'Часто задаваемые вопросы'")
-    public void FAQ_TEST() throws IOException {
-        HomePage homePage = open(ConfigProviderInterface.baseURL, HomePage.class);
-        homePage.AllQuestionsLink_Redirect();
-    }
-
-    @Test(priority = 10, description = "Домашняя страница. Проверка редиректа в блоке 'Новости'")
-    public void News_TEST() throws IOException {
-        HomePage homePage = open(ConfigProviderInterface.baseURL, HomePage.class);
-        homePage.AllNewsLink_Redirect();
-    }
 
 }
