f1da974ab62943a3bb5ca7b10f99fe9a73658d46
TheoRybalov <fedorrybalov@gmail.com>
2024-07-06 16:16:30 +0300

    Добавлен тестовый метод PROVIDERS.LayoutScreenshot_TEST для проверки вёрстки по макету. ProvidersPage.TakeScreenshotOfFullPage и ProvidersPage.compareScreenshotsOfFullPage для получения скриншота полной страницы. reference.png как макет страницы

--- src/test/java/Efficiency/Pages/ProvidersPage.java
+++ src/test/java/Efficiency/Pages/ProvidersPage.java
@@ -5,6 +5,9 @@ import com.codeborne.selenide.SelenideElement;
 import io.qameta.allure.Step;
 import org.testng.Assert;
 
+import java.io.IOException;
+
+import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selenide.$x;
 
 public class ProvidersPage extends CommonFunctions {
@@ -16,6 +19,11 @@ public class ProvidersPage extends CommonFunctions {
 
     //Вход для поставщика
     public static final SelenideElement LoginAsProviderLink = $x("//*[@id=\"root\"]/div/main/div/article/section[1]/div/div/div/div/div/div[2]/a");
+    public static final SelenideElement CookieButton = $x("//*[@id=\"rcc-confirm-button\"]");
+    @Step("Принимаем куки")
+    public void AddCookies(){
+        CookieButton.shouldBe(visible).click();
+    }
 
     @Step("Проверка редиректа по ссылке 'Заполнить анкету поставщика'")
     public void ProviderFormLink_Redirect(){
@@ -23,15 +31,61 @@ public class ProvidersPage extends CommonFunctions {
         super.Check_Redirect_By_Link(ProviderFormLink, "https://svi.ctp.devops.xn--b1afjhrgvdfla9hb.xn--p1ai/idm/Accounts/RegisterOrganizationMode?accountType=Vendor");
     }
 
-    @Step("")
+    @Step("Проверка редиректа по ссылке 'Стать поставщиком'")
     public void BecomeAProviderLink_Redirect(){
         Assert.assertEquals(BecomeAProviderLink.getText(), "Стать поставщиком", "Текст элемента не соответствует заданному");
         super.Check_Redirect_By_Link(BecomeAProviderLink, "https://svi.ctp.devops.xn--b1afjhrgvdfla9hb.xn--p1ai/idm/Accounts/RegisterOrganizationMode?accountType=Vendor");
     }
 
-    @Step("")
+    @Step("Проверка редиректа по ссылке 'Вход для поставщика'")
     public void LoginAsProviderLink_Redirect(){
         Assert.assertEquals(LoginAsProviderLink.getText(), "Вход для поставщика", "Текст элемента не соответствует заданному");
         super.Check_Redirect_By_Link(LoginAsProviderLink, "https://aksis.dev.qsupport.ru/partner");
+    }
+
+    @Step("Создание скриншота всей страницы")
+    public void TakeScreenshotOfFullPage(String environment) throws IOException {
+        String screenshotPath = null;
+
+        switch (environment) {
+            case "PC":
+                //Если мы получаем скриншот в кофигурации ПК, то сохранение будет идти в эту папку
+                screenshotPath = "src/test/resources/screenshots/ProvidersPage/PC/FullPage/current.png";
+                break;
+            case "phone":
+                //Если мы получаем скриншот в кофигурации телефон, то сохранение будет идти в эту папку
+                screenshotPath = "src/test/resources/screenshots/ProvidersPage/phone/FullPage/current.png";
+                break;
+            default:
+                throw new IllegalArgumentException("Неверный параметр окружения: " + environment);
+        }
+
+        //вызывается метод создания и сохранения такого скриншота, куда мы передаём путь до папки
+        super.TakeScreenshotOfFullPage(screenshotPath);
+    }
+
+    public boolean compareScreenshotsOfFullPage(String environment) throws IOException{
+        String screenshotPath = null;
+        String referencePath = null;
+        String resultPath = null;
+
+        switch (environment) {
+            case "PC":
+                //Если мы получаем скриншот в кофигурации ПК, то для сравнения current и reference нужно вытащить из этих папок
+                screenshotPath = "src/test/resources/screenshots/ProvidersPage/PC/FullPage/current.png";
+                referencePath = "src/test/resources/screenshots/ProvidersPage/PC/FullPage/reference.png";
+                resultPath = "src/test/resources/screenshots/ProvidersPage/PC/FullPage/differences.png";
+                break;
+            case "phone":
+                //Если мы получаем скриншот в кофигурации телефона, то для сравнения current и reference нужно вытащить из этих папок
+                screenshotPath = "src/test/resources/screenshots/ProvidersPage/phone/FullPage/current.png";
+                referencePath = "src/test/resources/screenshots/ProvidersPage/phone/FullPage/reference.png";
+                resultPath = "src/test/resources/screenshots/ProvidersPage/phone/FullPage/differences.png";
+                break;
+            default:
+                throw new IllegalArgumentException("Неверный параметр окружения: " + environment);
+        }
+        //вызывается сравнения скриншотов, куда мы передаём пути до папок
+        return super.compareScreenshots(screenshotPath, referencePath, resultPath);
     }
 }
--- src/test/java/Efficiency/Tests/PROVIDERS.java
+++ src/test/java/Efficiency/Tests/PROVIDERS.java
@@ -1,8 +1,10 @@
 package Efficiency.Tests;
 
+import Efficiency.Pages.HomePage;
 import Efficiency.Pages.ProvidersPage;
 import Efficiency.Providers.ConfigProviderInterface;
 import Efficiency.TestBase;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 import java.io.IOException;
@@ -10,6 +12,10 @@ import java.io.IOException;
 import static com.codeborne.selenide.Selenide.open;
 
 public class PROVIDERS extends TestBase {
+
+    public String getEnvironment(){
+        return super.getEnv();
+    }
     @Test(priority = 1, description = "Страница 'Поставщикам'. Редирект 'Заполнить анкету поставщика'")
     public void PerformanceLink_TEST() throws IOException {
         ProvidersPage providersPage = open(ConfigProviderInterface.providersURL, ProvidersPage.class);
@@ -26,5 +32,14 @@ public class PROVIDERS extends TestBase {
     public void LoginAsProviderLink_TEST() throws IOException {
         ProvidersPage providersPage = open(ConfigProviderInterface.providersURL, ProvidersPage.class);
         providersPage.LoginAsProviderLink_Redirect();
+    }
+
+    @Test(priority = 5, description = "Домашняя страница. Тест вёрстки через скриншот")
+    public void LayoutScreenshot_TEST() throws IOException {
+        ProvidersPage providersPage = open(ConfigProviderInterface.providersURL, ProvidersPage.class);
+        providersPage.AddCookies();
+        providersPage.TakeScreenshotOfFullPage(getEnvironment());
+        boolean ResultOfComparing = providersPage.compareScreenshotsOfFullPage(getEnvironment());
+        Assert.assertTrue(ResultOfComparing, "Скриншоты не совпали. Вёрстка не такая, как в макете");
     }
 }
