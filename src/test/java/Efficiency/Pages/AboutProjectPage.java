2da65fc0041843a5c07df5649f51d8af6cf99e3e
TheoRybalov <fedorrybalov@gmail.com>
2024-07-07 14:05:52 +0300

    В класс ABOUT_PROJECT добавлен метод ABOUT_PROJECT.LayoutScreenshot_TEST для проверки вёрстки по скриншоту

--- src/test/java/Efficiency/Pages/AboutProjectPage.java
+++ src/test/java/Efficiency/Pages/AboutProjectPage.java
@@ -1,6 +1,67 @@
 package Efficiency.Pages;
 
 import Efficiency.CommonFunctions;
+import com.codeborne.selenide.SelenideElement;
+import io.qameta.allure.Step;
+
+import java.io.IOException;
+
+import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Selenide.$x;
 
 public class AboutProjectPage extends CommonFunctions {
+
+    public static final SelenideElement CookieButton = $x("//*[@id=\"rcc-confirm-button\"]");
+
+
+    @Step("Принимаем куки")
+    public void AddCookies(){
+        CookieButton.shouldBe(visible).click();
+    }
+
+    @Step("Создание скриншота всей страницы")
+    public void TakeScreenshotOfFullPage(String environment) throws IOException {
+        String screenshotPath = null;
+
+        switch (environment) {
+            case "PC":
+                //Если мы получаем скриншот в кофигурации ПК, то сохранение будет идти в эту папку
+                screenshotPath = "src/test/resources/screenshots/AboutProjectPage/PC/FullPage/current.png";
+                break;
+            case "phone":
+                //Если мы получаем скриншот в кофигурации телефон, то сохранение будет идти в эту папку
+                screenshotPath = "src/test/resources/screenshots/AboutProjectPage/phone/FullPage/current.png";
+                break;
+            default:
+                throw new IllegalArgumentException("Неверный параметр окружения: " + environment);
+        }
+
+        //вызывается метод создания и сохранения такого скриншота, куда мы передаём путь до папки
+        super.TakeScreenshotOfFullPage(screenshotPath);
+    }
+
+    public boolean compareScreenshotsOfFullPage(String environment) throws IOException{
+        String screenshotPath = null;
+        String referencePath = null;
+        String resultPath = null;
+
+        switch (environment) {
+            case "PC":
+                //Если мы получаем скриншот в кофигурации ПК, то для сравнения current и reference нужно вытащить из этих папок
+                screenshotPath = "src/test/resources/screenshots/AboutProjectPage/PC/FullPage/current.png";
+                referencePath = "src/test/resources/screenshots/AboutProjectPage/PC/FullPage/reference.png";
+                resultPath = "src/test/resources/screenshots/AboutProjectPage/PC/FullPage/differences.png";
+                break;
+            case "phone":
+                //Если мы получаем скриншот в кофигурации телефона, то для сравнения current и reference нужно вытащить из этих папок
+                screenshotPath = "src/test/resources/screenshots/AboutProjectPage/phone/FullPage/current.png";
+                referencePath = "src/test/resources/screenshots/AboutProjectPage/phone/FullPage/reference.png";
+                resultPath = "src/test/resources/screenshots/AboutProjectPage/phone/FullPage/differences.png";
+                break;
+            default:
+                throw new IllegalArgumentException("Неверный параметр окружения: " + environment);
+        }
+        //вызывается сравнения скриншотов, куда мы передаём пути до папок
+        return super.compareScreenshots(screenshotPath, referencePath, resultPath);
+    }
 }
