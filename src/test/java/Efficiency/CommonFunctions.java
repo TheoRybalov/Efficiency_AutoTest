6f2e7b1a9e7ae29bf4fa2d04ea3df8283cb36d31
TheoRybalov <fedorrybalov@gmail.com>
2024-08-07 15:08:35 +0300

    Добавлены все текущие тесты в all.xml

--- src/test/java/Efficiency/CommonFunctions.java
+++ src/test/java/Efficiency/CommonFunctions.java
@@ -124,7 +124,7 @@ public class CommonFunctions {
         JavascriptExecutor js = (JavascriptExecutor) driver;
         js.executeScript("arguments[0].scrollIntoView();", element);
         js.executeScript("document.body.style.overflow = 'hidden';");
-        element.shouldBe(visible);
+        sleep(4000);
         File screenshotFile = element.screenshot();
 
         BufferedImage fullImg = ImageIO.read(screenshotFile);
